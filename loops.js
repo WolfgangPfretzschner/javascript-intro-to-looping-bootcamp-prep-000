3e3cb97d3e766da91189baf85ae0f0f8c033c8db
Wolfgang Pfretzschner <wolfgangp@live.com>
2018-03-29 10:06:32 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -2,9 +2,9 @@ function forLoop(array){
   array = [];
   for(var i = 0; i<25;i++){
     if(i===1){
-      array.push('I am 1 strange loop.')
+      array[i].push('I am 1 strange loop.')
     }else{
-      array.push(`I am ${i} strange loops.`)
+      array[i].push(`I am ${i} strange loops.`)
     }
   }
 }
