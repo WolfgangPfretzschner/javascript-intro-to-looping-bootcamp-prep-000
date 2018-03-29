954128e625654b6e71e0cd7ac4611a5a1bab4191
Wolfgang Pfretzschner <wolfgangp@live.com>
2018-03-29 10:57:34 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -25,7 +25,7 @@ function doWhileLoop(array){
   return Math.random() >= 0.5;
   }
   do{
-    array.length-1;
+    array.pop()
   }while (array.length>0 || maybeTrue()===false);
   
   return array
