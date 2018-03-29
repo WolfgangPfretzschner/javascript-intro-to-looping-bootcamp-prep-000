8dca4924ce130d691aa8647e80198cf67313a857
Wolfgang Pfretzschner <wolfgangp@live.com>
2018-03-29 10:52:43 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -23,7 +23,7 @@ function maybeTrue() {
 function doWhileLoop(array){
   do{
     array.length-1;
-  }while (array.length>0 || maybeTrue()==false);
+  }while (array.length>0 || maybeTrue()===false);
   
   return array
 }
